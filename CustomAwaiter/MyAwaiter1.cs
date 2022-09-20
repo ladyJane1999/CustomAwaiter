f3c5872e52b7ea0638746f0391cae30974f2e690
ladyJane1999 <anzhela.romanova.99@bk.ru>
2022-09-20 11:40:15 +0300

    Добавьте файлы проекта.

---
+++ CustomAwaiter/MyAwaiter1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomAwaiter
+{
+    public class MyObject
+    {
+        public MyAwaiter1 GetAwaiter()
+        {
+            return new MyAwaiter1();
+        }
+
+        public int SetCompleted()
+        {
+            return Thread.CurrentThread.ManagedThreadId;
+        }
+    }
+
+    public struct MyAwaiter1 : INotifyCompletion
+    {
+        public void OnCompleted(Action continuation)
+        {
+            continuation();
+        }
+        public bool IsCompleted { get; private set; }
+
+        public void GetResult()
+        {
+            Thread.Sleep(2000);
+        }
+    }
+}
---
+++ CustomAwaiter/Program.cs
@@ -0,0 +1,20 @@
+﻿using CustomAwaiter;
+
+public class Program
+{
+    static async Task Main(string[] args)
+    {
+        MyObject me = new MyObject();
+      
+        Console.WriteLine($"Run {Thread.CurrentThread.ManagedThreadId}");
+        _ = Task.Run(() =>
+        {
+            Thread.Sleep(1000);
+            Console.WriteLine($"SetCompleted {Thread.CurrentThread.ManagedThreadId}");
+            me.SetCompleted();
+        });
+        await me;
+        Console.WriteLine($"After await {Thread.CurrentThread.ManagedThreadId}");
+        Thread.Sleep(2000);
+    }
+}
